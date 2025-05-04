9467d159cb93f87f36f99a796452b9970b89354a
Nicolas Alves <nicolasalvessilva2006@gmail.com>
2025-05-04 18:14:58 -0300

    Atualização

--- 07 - importacao/exercicio3.js
+++ 07 - importacao/exercicio3.js
@@ -1,15 +1,13 @@
-//3) Faça um programa que receba a quantidade de números e seus respectivos valores.
-//Imprima todos os números pares.
-
-let numero = [5, 1 , 3 , 9 , 10 , 12]
-
+// exercicio3.js
+const entrada = [5, 1, 3, 9, 10, 12]; // Primeiro número é a quantidade de entradas
+let i = 0;
 
 function gets() {
-    const numerosparimpar = numero;
-    i++;
-    return numerosparimpar;
+    return entrada[i++]; // Retorna um item da entrada por vez
 }
+
 function print(texto) {
     console.log(texto);
 }
+
 module.exports = { gets, print };
--- 07 - importacao/exerciciomain3.js
+++ 07 - importacao/exerciciomain3.js
@@ -1,10 +1,10 @@
-//3) Faça um programa que receba a quantidade de números e seus respectivos valores.
-//Imprima todos os números pares.
 const { gets, print } = require('./exercicio3.js');
 
-const numero = []; // Array para armazenar os números
-const numeros = Number(gets()); // Número fixo para a tabuada
-numeros = gets(); // Número de entradas
-numero.push(Number(numeros)); // Adiciona ao array convertendo para número
+const numeroentrada = Number(gets());
 
-print(numero); // Imprime o array de números
+for (let i = 0; i < numeroentrada; i++) {
+    const numero = Number(gets());
+    if (numero % 2 === 0) {
+        print(numero);
+    }
+}
